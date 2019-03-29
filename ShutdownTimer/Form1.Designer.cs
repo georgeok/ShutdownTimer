ec7270e0425e9b34da6c6f776f12b1e96e6bfaa8
georgeok <giorgos.n.oikonomou@gmail.com>
2019-03-29 19:10:51 +0000

    Initial commit

---
+++ ShutdownTimer/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ShutdownTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.lbl_remaining = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_shutdown = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(105, 13);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            480,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(103, 26);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numericUpDown1_KeyPress);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 104);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(470, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(450, 16);
+            // 
+            // lbl_remaining
+            // 
+            this.lbl_remaining.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_remaining.AutoSize = true;
+            this.lbl_remaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_remaining.Location = new System.Drawing.Point(43, 54);
+            this.lbl_remaining.Name = "lbl_remaining";
+            this.lbl_remaining.Size = new System.Drawing.Size(93, 33);
+            this.lbl_remaining.TabIndex = 8;
+            this.lbl_remaining.Text = "label1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown1);
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_remaining);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btn_shutdown);
+            this.splitContainer1.Panel2.Controls.Add(this.btn_cancel);
+            this.splitContainer1.Size = new System.Drawing.Size(470, 104);
+            this.splitContainer1.SplitterDistance = 287;
+            this.splitContainer1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Turn Off in:";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(214, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "minutes";
+            // 
+            // btn_shutdown
+            // 
+            this.btn_shutdown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_shutdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_shutdown.ForeColor = System.Drawing.Color.Red;
+            this.btn_shutdown.Image = global::ShutdownTimer.Properties.Resources.if_flat_style_circle_turn_on_off_1312510;
+            this.btn_shutdown.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_shutdown.Location = new System.Drawing.Point(3, 53);
+            this.btn_shutdown.Name = "btn_shutdown";
+            this.btn_shutdown.Size = new System.Drawing.Size(164, 44);
+            this.btn_shutdown.TabIndex = 1;
+            this.btn_shutdown.Text = "Shutdown";
+            this.btn_shutdown.UseVisualStyleBackColor = true;
+            this.btn_shutdown.Click += new System.EventHandler(this.btn_shutdown_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cancel.Image = global::ShutdownTimer.Properties.Resources.if_Artboard_1_1790660;
+            this.btn_cancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_cancel.Location = new System.Drawing.Point(3, 3);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(164, 44);
+            this.btn_cancel.TabIndex = 4;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(470, 126);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_shutdown;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Label lbl_remaining;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ShutdownTimer/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShutdownTimer
+{
+    public partial class Form1 : Form
+    {
+        private DateTime shutdownDate = DateTime.Now;
+        private bool timerRunning = false;
+        private double totalSeconds = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_shutdown_Click(object sender, EventArgs e)
+        {
+            startTurnOff();
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            disableControls(false);
+            timerRunning = false;
+            updateRemainingLabel();
+            this.numericUpDown1.Focus();
+        }
+
+        private void disableControls(bool yesNo)
+        {
+            btn_shutdown.Enabled = !yesNo;
+            numericUpDown1.Enabled = !yesNo;
+        }
+
+        private void startTurnOff()
+        {
+            Properties.Settings.Default.LastTimer = Convert.ToInt32(numericUpDown1.Value);
+            Properties.Settings.Default.Save();
+            disableControls(true);
+            double minutes = Convert.ToDouble(this.numericUpDown1.Value);
+            this.shutdownDate = DateTime.Now.Add(TimeSpan.FromMinutes(minutes));
+            timerRunning = true;
+            totalSeconds = this.shutdownDate.Subtract(DateTime.Now).TotalSeconds;
+        }
+
+        private void stopTurnOff()
+        {
+            timerRunning = false;
+            // Standby
+            //Application.SetSuspendState(PowerState.Suspend, true, true);
+            Process.Start("shutdown", "/s /t 0");  // shutdown
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Shutdown Timer - " + DateTime.Now.ToString("HH:mm:ss");
+            if (timerRunning)
+            {
+                var diff = this.shutdownDate.Subtract(DateTime.Now);
+                this.lbl_remaining.Text = String.Format("{0:0} min {1:0} sec",
+                    Math.Floor(diff.TotalSeconds / 60),
+                    diff.TotalSeconds % 60
+                    );
+                toolStripProgressBar1.Value = (int)((totalSeconds - diff.TotalSeconds) / totalSeconds * 100);
+                if (diff.TotalSeconds < 1)
+                {
+                    stopTurnOff();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Interval = 1000;
+            timer.Start();
+            numericUpDown1.Value = Properties.Settings.Default.LastTimer;
+            updateRemainingLabel();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            updateRemainingLabel();
+        }
+
+        private void updateRemainingLabel()
+        {
+            double minutes = Convert.ToDouble(this.numericUpDown1.Value);
+            this.lbl_remaining.Text = String.Format("{0:0} min {1:0} sec", minutes, 0);
+            toolStripProgressBar1.Value = 100;
+        }
+
+        private void numericUpDown1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                startTurnOff();
+            }
+        }
+    }
+}
